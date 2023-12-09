ac58f3aa7bcf781fceeb09d68b8bd271745d0ce1
Vencislav <vencislav67@hotmail.com>
2023-12-09 11:39:28 +0200

    10 done

---
+++ 10_OddEvenSum/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+int sum1 = 0;
+int sum2 = 0;
+int br = 1;
+for (int i = 0; i < n; i++)
+{
+
+    int m = int.Parse(Console.ReadLine());
+    if (br % 2 == 1)
+    {
+        sum1 = sum1 + m;
+    }
+    else
+    {
+        sum2 = sum2 + m;
+    }
+    br++;
+}
+if (sum1 == sum2)
+{
+    Console.WriteLine($"Yes");
+    Console.WriteLine($"Sum = {sum1}");
+}
+else
+{
+    Console.WriteLine($"No");
+    Console.WriteLine($"Diff = {Math.Abs(sum1 - sum2)}");
+}
