a673043ef99f833a1a0335b52338795ae6590294
Vencislav <vencislav67@hotmail.com>
2023-12-09 11:37:33 +0200

    8 task done

---
+++ 8_NumSeq/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+int min = 0;
+int max = 0;
+for (int i = 0; i < n; i++)
+{
+    int m = int.Parse(Console.ReadLine());
+    if (i == 0)
+    {
+        min = m;
+        max = m;
+    }
+    if (m > max)
+    {
+        max = m;
+    }
+    if (m < min)
+    {
+        min = m;
+    }
+
+}
+Console.WriteLine($"Max number: {max}");
+Console.WriteLine($"Min number: {min}");
