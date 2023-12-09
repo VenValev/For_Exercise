5392f61ec95cbfa664b9af768996c6c0f72c82ee
Vencislav <vencislav67@hotmail.com>
2023-12-09 11:47:03 +0200

    18

---
+++ 18_TenisRanklist/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+double p = double.Parse(Console.ReadLine());
+double br = 0;
+double avp = 0;
+for (int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+    switch (s)
+    {
+        case "W":
+            p += 2000;
+            avp += 2000;
+            br++;
+            break;
+        case "F":
+            p += 1200;
+            avp += 1200;
+            break;
+        case "SF":
+            p += 720;
+            avp += 720;
+            break;
+    }
+}
+Console.WriteLine($"Final points: {p}");
+Console.WriteLine($"Average points: {Math.Floor(avp / n)}");
+Console.WriteLine($"{(br / n) * 100.0:f2}%");
