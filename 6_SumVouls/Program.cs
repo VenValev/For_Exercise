3ce42792caf998eb8345ce26f27f1a7e4997efe7
Vencislav <vencislav67@hotmail.com>
2023-12-09 11:35:11 +0200

    task 6 complete

---
+++ 6_SumVouls/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _6_Sum_vowls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                
+                if (c == 'a')
+                {
+                    sum += 1;
+                }
+                if (c == 'e')
+                {
+                    sum += 2;
+                }
+                if (c == 'i')
+                {
+                    sum += 3;
+                }
+                if (c == 'o')
+                {
+                    sum += 4;
+                }
+                if (c == 'u')
+                {
+                    sum += 5;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
